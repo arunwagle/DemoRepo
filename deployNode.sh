a2e5e1f7fa6a3804883eba63d801c1d98884f105
Arun Wagle <arunwagle@Aruns-MBP.fios-router.home>
2017-09-08 15:22:30 -0400

    changes deployNode script

--- deployNode.sh
+++ deployNode.sh
@@ -4,7 +4,7 @@ cd IBM/aw-demo-app
 
 bluemix api https://api.ng.bluemix.net
 
-bluemix login -u arun.wagle@ibm.com -o OSNorth.Arun.Wagle.Org -s Development
+bluemix login -u arun.wagle@ibm.com -o OSNorth.Arun.Wagle.Org -s Development -p sourceCode0520
 
 bluemix app push aw-demo-app
 
